6a0aefe0d3a9dd640911aa652ece82271535b09f
MAURO RODRIGO SPINETTI <mauro.rspinetti@senacsp.edu.br>
2017-11-06 15:43:18 -0200

    com tela de introdução

---
+++ Assets/Scripts/ExplosaoAnimatorControllerHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosaoAnimatorControllerHelper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TerminouAnimacao()
+    {
+        GameController gc = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+        gc.OnGameOver();
+    }
+}
---
+++ Assets/Scripts/TelaIntroController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TelaIntroController : MonoBehaviour {
+
+    public Text hudRecord;
+
+	// Use this for initialization
+	void Start () {
+
+        MostraRecord();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MostraRecord()
+    {
+        if (PlayerPrefs.HasKey("RECORDE"))
+            hudRecord.text = string.Format("RECORDE: {0}", PlayerPrefs.GetInt("RECORDE"));
+        else
+            hudRecord.text = "RECORD: NÃO HÁ RECORDES";
+    }
+
+    public void LimpaRecorde()
+    {
+        if (PlayerPrefs.HasKey("RECORDE"))
+        {
+            PlayerPrefs.DeleteKey("RECORDE");
+            hudRecord.text = "RECORDE: NÃO HÁ RECORDES";
+        }
+    }
+}
